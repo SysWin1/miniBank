3d9dd336431de91c57744796b7cdeee0e83049e2
HJunng <guswjd1gh@gmail.com>
2024-06-28 20:10:30 +0900

    refactor : 출력 포맷 수정

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,8 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         OUT:
         while(true){
+            System.out.println();
+            System.out.println("===== 업무 가능 내용 =====");
             System.out.println("1. 계좌 생성");
             System.out.println("2. 계좌 정보조회");
             System.out.println("3. 소유자별 계좌 목록 조회");
@@ -31,22 +33,24 @@ public class Main {
                     System.out.println("업무를 종료합니다.");
                     break OUT;
                 case 1:
-                    System.out.println("생성할 계좌 번호를 입력하세요: ");
+                    System.out.print("생성할 계좌 번호를 입력하세요: ");
                     String accountNo = br.readLine();
-                    System.out.println("고객 명을 입력하세요: ");
+                    System.out.print("고객 명을 입력하세요: ");
                     String name = br.readLine();
                     bank.addAccount(accountNo,name);
                     break;
                 case 2:
-                    System.out.println("조회할 계좌 번호를 입력하세요: ");
-                    String accountInfo = br.readLine();
-                    Account account = bank.getAccount(accountInfo);
+                    System.out.print("조회할 계좌 번호를 입력하세요: ");
+                    String accountNum = br.readLine();
+                    Account account = bank.getAccount(accountNum);
+                    System.out.println("== 해당 계좌번호의 계좌정보 ==");
                     System.out.println(account.toString());
                     break;
                 case 3:
-                    System.out.println("고객명을 입력하세요: ");
-                    String findforName = br.readLine();
-                    bank.findAccount(findforName);
+                    System.out.print("고객명을 입력하세요: ");
+                    String userName = br.readLine();
+                    System.out.println("== 해당 소유자명의 계좌 목록 ==");
+                    bank.findAccount(userName);
                     break;
                 case 4:
                     bank.getAccounts();
@@ -55,34 +59,35 @@ public class Main {
                     System.out.println("총 계좌 수는 "+bank.getTotalAccount() + "입니다.");
                     break;
                 case 6:
-                    System.out.println("입금할 계좌 번호를 입력하세요: ");
-                    String depositAccount = br.readLine();
-                    System.out.println("입금할 금액을 입력해 주세요: ");
+                    System.out.print("입금할 계좌 번호를 입력하세요: ");
+                    accountNum = br.readLine();
+                    System.out.print("입금할 금액을 입력해 주세요: ");
                     Long depositAmount = Long.valueOf(br.readLine());
-                    Account accountd = bank.getAccount(depositAccount);
-                    long balance = accountd.getBalance();
-                    accountd.deposit(depositAmount);
+                    account = bank.getAccount(accountNum);
+                    long balance = account.getBalance();
+                    account.deposit(depositAmount);
                     break;
                 case 7:
-                    System.out.println("출금할 계좌 번호를 입력하세요: ");
-                    String withdrawAccount = br.readLine();
-                    System.out.println("출금할 금액을 입력해 주세요: ");
+                    System.out.print("출금할 계좌 번호를 입력하세요: ");
+                    accountNum = br.readLine();
+                    System.out.print("출금할 금액을 입력해 주세요: ");
                     Long withdrawAmount = Long.valueOf(br.readLine());
-                    Account account1 = bank.getAccount(withdrawAccount);
-                    long balance1 = account1.getBalance();
-                    account1.withdraw(withdrawAmount);
+                    account = bank.getAccount(accountNum);
+                    balance = account.getBalance();
+                    account.withdraw(withdrawAmount);
                     break;
                 case 8:
-                    System.out.println("잔액 조회할 계좌 번호를 입력하세요: ");
+                    System.out.print("잔액 조회할 계좌 번호를 입력하세요: ");
                     String balanceInfo = br.readLine();
                     Account balanceAccount = bank.getAccount(balanceInfo);
                     System.out.println("현재 잔액은 "+balanceAccount.getBalance() + "원 입니다.");
                     break;
                 case 9:
-                    System.out.println("거래내역 조회할 계좌 번호를 입력하세요: ");
+                    System.out.print("거래내역 조회할 계좌 번호를 입력하세요: ");
                     String transactionInfo = br.readLine();
                     Account transactionAccount = bank.getAccount(transactionInfo);
                     List<Transaction> transactions = transactionAccount.getTransactions();
+                    System.out.println("== 거래 내역 ==");
                     for(Transaction t:transactions){
                         System.out.println(t.toString());
                     }
