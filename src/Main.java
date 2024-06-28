9ef59c9652e05b88ccc345d784800cda8e366cfb
SSooya <tvfxq0214@gmail.com>
2024-06-28 17:31:27 +0900

    feature: 업무케이스 생성

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,49 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner sc = new Scanner(System.in);
+
+        OUT:
+        while(true){
+            System.out.println("1. 계좌 생성");
+            System.out.println("2. 계좌 정보조회");
+            System.out.println("3. 소유자별 계좌 목록 조회");
+            System.out.println("4. 전계좌 조회");
+            System.out.println("5. 총 계좌수 반환");
+            System.out.println("6. 입금");
+            System.out.println("7. 출금");
+            System.out.println("8. 잔액조회");
+            System.out.println("9. 거래내역 조회");
+            System.out.println("0. 업무 종료");
+            System.out.print("진행하고자 하는 업무를 입력해 주세요: ");
+            int work = sc.nextInt();
+            switch (work) {
+                case 0:
+                    System.out.println("업무를 종료합니다.");
+                    break OUT;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                default:
+                    System.out.println("잘못된 번호를 입력하였습니다. 다시 입력해 주세요");
+            }
+        }
     }
 }
