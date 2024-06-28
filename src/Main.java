bff68ea524b9bc78c698849f6a9ab8ad32818be2
SSooya <tvfxq0214@gmail.com>
2024-06-28 18:52:09 +0900

    feature: 계좌생성 업무 추가

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Bank bank;
 
-        Scanner sc = new Scanner(System.in);
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         OUT:
         while(true){
             System.out.println("1. 계좌 생성");
@@ -18,12 +21,18 @@ public class Main {
             System.out.println("9. 거래내역 조회");
             System.out.println("0. 업무 종료");
             System.out.print("진행하고자 하는 업무를 입력해 주세요: ");
-            int work = sc.nextInt();
+            int work = Integer.parseInt(br.readLine());
+            System.out.println();
             switch (work) {
                 case 0:
                     System.out.println("업무를 종료합니다.");
                     break OUT;
                 case 1:
+                    System.out.println("생성할 계좌 번호를 입력하세요: ");
+                    String accountNo = br.readLine();
+                    System.out.println("고객 명을 입력하세요: ");
+                    String name = br.readLine();
+                    bank.addAccount(accountNo,name);
                     break;
                 case 2:
                     break;
